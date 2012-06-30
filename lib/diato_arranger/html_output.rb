8a9c5d130b9a247c72f28a1bb501950b750d3a1b
David Heath <david@davidheath.org>
2012-06-30 12:02:02 +0100

    Adjust vertical position according to pitch.

--- lib/diato_arranger/html_output.rb
+++ lib/diato_arranger/html_output.rb
@@ -47,7 +47,7 @@ module DiatoArranger
         when PlayableNote then
           "<div class='note #{duration_class(playing_instruction.duration)}'>" +
             "<span class='accompaniment #{playing_instruction.direction}'>#{playing_instruction.accompaniment}</span>" +
-            "<span class='duration #{playing_instruction.direction}'>#{duration_symbol(playing_instruction.duration)}</span>" +
+            "<span class='duration #{playing_instruction.direction}' style='top: #{50 - playing_instruction.note.pitch_number*2}px'>#{duration_symbol(playing_instruction.duration)}</span>" +
             "<span class='instruction #{playing_instruction.direction} #{playing_instruction.row}'>#{playing_instruction.button_number}</span>" +
             "<span class='letter #{playing_instruction.direction}'>#{playing_instruction.note.to_s(false)}</span>" +
           "</div>\n"
