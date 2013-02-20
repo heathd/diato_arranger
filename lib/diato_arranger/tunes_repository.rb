36588a666ebd376d77269f3285c4b5147845e39c
David Heath <david@davidheath.org>
2013-02-20 21:27:40 +0000

    Ensure tunes dir gets created

--- lib/diato_arranger/tunes_repository.rb
+++ lib/diato_arranger/tunes_repository.rb
@@ -1,7 +1,13 @@
 require 'digest/md5'
+require 'fileutils'
 
 module DiatoArranger
   class TunesRepository < Struct.new(:path)
+    def initialize(*args)
+      super
+      FileUtils.mkdir_p(path)
+    end
+
     def create(abc)
       hash = Digest::MD5.hexdigest(abc)
       File.open(filename(hash), "w") { |f| f.write(abc) }
